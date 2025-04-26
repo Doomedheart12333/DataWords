f66e9347e2bc9986bca2185811176273bc4d0644
Doomedheart12333 <131315688+Doomedheart12333@users.noreply.github.com>
2025-04-25 21:50:49 -0500

    Update BAD.lua

--- BAD.lua
+++ BAD.lua
@@ -1,1 +1,13 @@
-print("This file was retrived from github with a loadstring!")
+local HttpService = game:GetService("HttpService")
+
+local url = "https://raw.githubusercontent.com/Doomedheart12333/DataWords/refs/heads/main/BAD.lua"
+
+local success, response = pcall(function()
+	return HttpService:GetAsync(url)
+end)
+
+if success then
+	loadstring(response)() -- this will print: print("This file was retrived from github with a loadstring!")
+else
+	warn("Failed to retrieve file:", response)
+end
